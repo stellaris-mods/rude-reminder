259a1525bc390394b6661e3c48873d99bdf0110d
folknor <folk@folk.wtf>
2017-09-22 19:02:20 +0200

    Bump to 1.8.0.

--- modinfo.lua
+++ modinfo.lua
@@ -4,7 +4,7 @@ return {
 	name = "Rude Reminder",
 	tags = { "Events", "Utilities", "Diplomacy" },
 	picture = "thumb.png",
-	supported_version = "1.6.*",
+	supported_version = "1.8.*",
 	remote_file_id = 818770380,
 	readme = "readme.md",
 	steambb = "steam.bbcode",
