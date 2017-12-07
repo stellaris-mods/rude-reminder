ff6d6f1f1c068e8039027cb86c6af83e123c2807
Folk <vhaarr@gmail.com>
2017-12-07 17:31:37 +0100

    Bump to 1.9.x and set original uploader.

--- modinfo.lua
+++ modinfo.lua
@@ -4,8 +4,9 @@ return {
 	name = "Rude Reminder",
 	tags = { "Events", "Utilities", "Diplomacy" },
 	picture = "thumb.png",
-	supported_version = "1.8.*",
+	supported_version = "1.9.*",
 	remote_file_id = 818770380,
+	originalUploader = "folk",
 	readme = "readme.md",
 	steambb = "steam.bbcode",
 }
